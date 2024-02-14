a773a8ab16f9c97e5f8376622d988a7a180184b5
Jonah <kayiwa.jonah@student.moringaschool.com>
2024-02-14 19:28:24 +0300

    first commit

---
+++ src/components/singleBot.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+function SingleBot({ bot, enlistedBots, setEnlistedBots }) {
+  const isEnlisted = enlistedBots.some(
+    (enlistedBot) => enlistedBot.id === bot.id
+  );
+
+  function handleDischarge(){
+    if (isEnlisted) {
+      
+      const updatedEnlistedBots = enlistedBots.filter(
+        (enlistedBot) => enlistedBot.id !== bot.id
+      );
+      setEnlistedBots(updatedEnlistedBots);
+    } else {
+      
+      setEnlistedBots([...enlistedBots, bot]);
+
+      fetch(`http://localhost:3000/bots/${bot.id}`, {
+        method: "DELETE",
+      })
+        .then((resp) => {
+          if (resp.status === 200) {
+            // Bot deleted successfully
+          } else if (resp.status === 404) {
+            // Handle the case where the bot is not found
+          } else {
+            // Handle error
+          }
+        })
+        .catch((error) => {
+          console.error("Error deleting bot:", error);
+        });
+    }
+  };
+
+  return (
+    <div 
+    className="single bot-card" onClick={handleDischarge}>
+      <h2>{bot.name}</h2>
+      
+      <img src={bot.avatar_url} alt="bots"/>
+      <p>HWALTH:{bot.health}</p>
+      <p>DAMAGE:{bot.damage}</p>
+      <p>ARMOR:{bot.armor}</p>
+      
+      {isEnlisted && (
+        <button className="discharge-button" onClick={handleDischarge}>
+        X
+      </button>
+      )}
+    </div>
+
+    
+  );
+}
+
+export default SingleBot;
